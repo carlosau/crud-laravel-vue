ba6405826cdedcdc9cd9b65e9cbab64fd2b47f35
Carlos Souza <carlossouza@Confsistemas-MacBook-Air.local>
2023-07-24 18:25:38 -0300

    added the validation for update skills

--- app/Http/Requests/StoreSkillRequest.php
+++ app/Http/Requests/StoreSkillRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Unique;
 
 class StoreSkillRequest extends FormRequest
 {
@@ -24,7 +25,7 @@ class StoreSkillRequest extends FormRequest
         return [
             //
             'name' => ['required', 'min:3', 'max:20'],
-            'slug' => ['required', 'unique:skills,slug,' . $this->skill->id]
+            'slug' => ['required', (new Unique('skills'))->ignore($this->skill)]
         ];
     }
 }
